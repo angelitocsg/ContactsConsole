a2fd7a24b65e037c641e8db3e467f52a038561f9
Angelito Casagrande <angelito@angelito.com.br>
2021-01-16 13:26:53 -0300

    Code Refactory

--- src/Contacts/Controllers/ContactsController.cs
+++ src/Contacts/Controllers/ContactsController.cs
@@ -2,8 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Contacts.Entities;
+using Contacts.Enumerations;
 using Contacts.Interfaces;
-using Contacts.ValueObjects;
 using Contacts.Views;
 
 namespace Contacts.Controllers
@@ -44,33 +44,41 @@ namespace Contacts.Controllers
 
         private void MenuActions(ConsoleKeyInfo action)
         {
-            switch (action.KeyChar)
+            switch ((MenuAction)action.KeyChar)
             {
-                case '1':
+                case MenuAction.List:
                     ListAll();
                     break;
 
-                case '2':
+                case MenuAction.Add:
                     Add();
                     break;
 
-                case '3':
+                case MenuAction.Edit:
                     Edit();
                     break;
 
-                case '4':
+                case MenuAction.Remove:
                     Remove();
                     break;
 
-                case '5':
+                case MenuAction.Search:
                     ListAll(true);
+                    break;
+
+                case MenuAction.WhatsApp:
+                    OpenLink(LinkType.WhatsApp);
+                    break;
+
+                case MenuAction.GoogleMaps:
+                    OpenLink(LinkType.GoogleMaps);
                     break;
             }
         }
 
         private void Remove()
         {
-            var id = GetId();
+            var id = View.GetId();
 
             var contact = _repository.GetByPartialId(id);
 
@@ -85,24 +93,6 @@ namespace Contacts.Controllers
                 View.InitialScreen();
                 View.ShowStatusBarMessage($"Contact with id {id} removed.");
             }
-        }
-
-        private void CreateForm()
-        {
-            var baseline = 5;
-            var currentLine = baseline;
-            var labelWidth = 15;
-
-            View.InitialScreen();
-
-            View.CreateTextAtLine(currentLine++, $"{"Name".PadRight(labelWidth)}:");
-            View.CreateTextAtLine(currentLine++, $"{"Cellphone".PadRight(labelWidth)}:");
-            View.CreateTextAtLine(currentLine++, $"{"Email".PadRight(labelWidth)}:");
-            View.CreateTextAtLine(currentLine++, $"{"Street".PadRight(labelWidth)}:");
-            View.CreateTextAtLine(currentLine++, $"{"Number".PadRight(labelWidth)}:");
-            View.CreateTextAtLine(currentLine++, $"{"Neighborhood".PadRight(labelWidth)}:");
-            View.CreateTextAtLine(currentLine++, $"{"City".PadRight(labelWidth)}:");
-            View.CreateTextAtLine(currentLine++, $"{"State (2 chars)".PadRight(labelWidth)}:");
         }
 
         private void PopulateForm(Contact contact)
@@ -137,83 +127,6 @@ namespace Contacts.Controllers
             Console.Write(contact.Address.State);
         }
 
-        private Contact UserInput(bool update = false, Contact contact = null)
-        {
-            var baseline = 5;
-            var currentLine = baseline;
-            var labelWidth = 15;
-            var inputStart = labelWidth + 4;
-
-            currentLine = baseline;
-            Console.SetCursorPosition(inputStart, currentLine++);
-            var name = Console.ReadLine();
-            name = update && string.IsNullOrWhiteSpace(name) ? contact.Name : name;
-
-            Console.SetCursorPosition(inputStart, currentLine++);
-            var cellphone = Console.ReadLine() ?? contact.Cellphone.ToString();
-            cellphone = update && string.IsNullOrWhiteSpace(cellphone) ? contact.Cellphone.ToString() : cellphone;
-
-            Console.SetCursorPosition(inputStart, currentLine++);
-            var email = Console.ReadLine() ?? contact.Email.ToString();
-            email = update && string.IsNullOrWhiteSpace(email) ? contact.Email.ToString() : email;
-
-            Console.SetCursorPosition(inputStart, currentLine++);
-            var street = Console.ReadLine() ?? contact.Address.Street;
-            street = update && string.IsNullOrWhiteSpace(street) ? contact.Address.Street : street;
-
-            Console.SetCursorPosition(inputStart, currentLine++);
-            int number = 0;
-            var numberStr = Console.ReadLine();
-            numberStr = update && string.IsNullOrWhiteSpace(numberStr) ? contact.Address.Number.ToString() : numberStr;
-            int.TryParse(numberStr, out number);
-
-            Console.SetCursorPosition(inputStart, currentLine++);
-            var neighborhood = Console.ReadLine() ?? contact.Address.Neighborhood;
-            neighborhood = update && string.IsNullOrWhiteSpace(neighborhood) ? contact.Address.Neighborhood : neighborhood;
-
-            Console.SetCursorPosition(inputStart, currentLine++);
-            var city = Console.ReadLine() ?? contact.Address.City;
-            city = update && string.IsNullOrWhiteSpace(city) ? contact.Address.City : city;
-
-            Console.SetCursorPosition(inputStart, currentLine++);
-            var state = Console.ReadLine() ?? contact.Address.State;
-            state = update && string.IsNullOrWhiteSpace(state) ? contact.Address.State : state;
-
-            if (update)
-            {
-                try
-                {
-                    var contactUpdate = new Contact(contact.Id,
-                        name,
-                        new Cellphone(cellphone),
-                        new Email(email),
-                        new Address(street, number, neighborhood, city, state));
-
-                    return contactUpdate;
-                }
-                catch (Exception ex)
-                {
-                    View.ShowStatusBarMessage(ex.Message);
-                }
-            }
-
-            try
-            {
-                var contactAdd = Contact.New(name,
-                    new Cellphone(cellphone),
-                    new Email(email),
-                    new Address(street, number, neighborhood, city, state));
-
-                return contactAdd;
-            }
-            catch (Exception ex)
-            {
-                View.ShowStatusBarMessage(ex.Message);
-            }
-
-            return null;
-        }
-
         private void Add()
         {
             var baseline = 5;
@@ -221,9 +134,9 @@ namespace Contacts.Controllers
             var labelWidth = 15;
             var inputStart = labelWidth + 4;
 
-            CreateForm();
+            View.CreateForm();
 
-            var contact = UserInput();
+            var contact = View.UserInput();
 
             if (contact == null)
                 return;
@@ -241,37 +154,10 @@ namespace Contacts.Controllers
             }
         }
 
-        private string GetId()
-        {
-            var baseline = 5;
-            var labelWidth = 10;
-            var inputStart = labelWidth + 4;
-
-            View.InitialScreen();
-
-            View.CreateTextAtLine(baseline, $"{"Enter Id".PadRight(labelWidth)}:");
-
-            Console.SetCursorPosition(inputStart, baseline);
-            return Console.ReadLine();
-        }
-
-        private string GetSearchText()
-        {
-            var baseline = 5;
-            var labelWidth = 25;
-            var inputStart = labelWidth + 4;
-
-            View.InitialScreen();
-
-            View.CreateTextAtLine(baseline, $"{"Enter text to search".PadRight(labelWidth)}:");
-
-            Console.SetCursorPosition(inputStart, baseline);
-            return Console.ReadLine();
-        }
 
         private void Edit()
         {
-            var id = GetId();
+            var id = View.GetId();
 
             var contact = _repository.GetByPartialId(id);
 
@@ -281,10 +167,10 @@ namespace Contacts.Controllers
                 return;
             }
 
-            CreateForm();
+            View.CreateForm();
             PopulateForm(contact);
 
-            var contactUpdated = UserInput(true, contact);
+            var contactUpdated = View.UserInput(true, contact);
 
             if (contactUpdated == null)
                 return;
@@ -298,7 +184,7 @@ namespace Contacts.Controllers
 
         private IEnumerable<Contact> Search()
         {
-            var searchText = GetSearchText();
+            var searchText = View.GetSearchText();
             return _repository.GetAllBySearch(searchText);
         }
 
@@ -320,5 +206,34 @@ namespace Contacts.Controllers
             View.CreateSingleMiddleLine(false); line++;
             contacts.ToList().ForEach(it => View.ShowAsList(baseline, line++, it.ToString()));
         }
+
+        private void OpenLink(LinkType linkType)
+        {
+            var id = View.GetId();
+
+            var contact = _repository.GetByPartialId(id);
+
+            if (contact == null)
+            {
+                View.ShowStatusBarMessage($"No contacts found with partial id {id}.");
+                return;
+            }
+
+            View.InitialScreen();
+
+            switch (linkType)
+            {
+                case LinkType.GoogleMaps:
+                    View.ShowStatusBarMessage($"Opening Google Maps for contact address.");
+                    _openLink.GoogleMaps(contact.Address);
+                    break;
+
+                case LinkType.WhatsApp:
+                    View.ShowStatusBarMessage($"Opening WhatsApp chat for contact cellphone.");
+                    _openLink.WhatsAppChat(contact.Cellphone);
+                    break;
+            }
+        }
+
     }
 }
--- src/Contacts/Views/MainView.cs
+++ src/Contacts/Views/MainView.cs
@@ -1,4 +1,6 @@
 using System;
+using Contacts.Entities;
+using Contacts.ValueObjects;
 
 namespace Contacts.Views
 {
@@ -16,7 +18,7 @@ namespace Contacts.Views
             CreateTopLine();
             CreateTextLine("CONTACT LIST");
             CreateMiddleLine();
-            CreateTextLine("1) List  2) Add  3) Edit  4) Delete  5) Search");
+            CreateTextLine("1) List  2) Add  3) Edit  4) Del  5) Search  6) WhatsApp  7) Maps");
             CreateSingleMiddleLine();
 
             for (int i = Lines + 3; i < SCREEN_HEIGHT; i++)
@@ -32,6 +34,129 @@ namespace Contacts.Views
         public void SetInputPosition()
         {
             Console.SetCursorPosition(70, 3);
+        }
+
+        public void CreateForm()
+        {
+            var baseline = 5;
+            var currentLine = baseline;
+            var labelWidth = 15;
+
+            InitialScreen();
+
+            CreateTextAtLine(currentLine++, $"{"Name".PadRight(labelWidth)}:");
+            CreateTextAtLine(currentLine++, $"{"Cellphone".PadRight(labelWidth)}:");
+            CreateTextAtLine(currentLine++, $"{"Email".PadRight(labelWidth)}:");
+            CreateTextAtLine(currentLine++, $"{"Street".PadRight(labelWidth)}:");
+            CreateTextAtLine(currentLine++, $"{"Number".PadRight(labelWidth)}:");
+            CreateTextAtLine(currentLine++, $"{"Neighborhood".PadRight(labelWidth)}:");
+            CreateTextAtLine(currentLine++, $"{"City".PadRight(labelWidth)}:");
+            CreateTextAtLine(currentLine++, $"{"State (2 chars)".PadRight(labelWidth)}:");
+        }
+
+        public string GetId()
+        {
+            var baseline = 5;
+            var labelWidth = 10;
+            var inputStart = labelWidth + 4;
+
+            InitialScreen();
+
+            CreateTextAtLine(baseline, $"{"Enter Id".PadRight(labelWidth)}:");
+
+            Console.SetCursorPosition(inputStart, baseline);
+            return Console.ReadLine();
+        }
+
+        public string GetSearchText()
+        {
+            var baseline = 5;
+            var labelWidth = 25;
+            var inputStart = labelWidth + 4;
+
+            InitialScreen();
+
+            CreateTextAtLine(baseline, $"{"Enter text to search".PadRight(labelWidth)}:");
+
+            Console.SetCursorPosition(inputStart, baseline);
+            return Console.ReadLine();
+        }
+
+        public Contact UserInput(bool update = false, Contact contact = null)
+        {
+            var baseline = 5;
+            var currentLine = baseline;
+            var labelWidth = 15;
+            var inputStart = labelWidth + 4;
+
+            currentLine = baseline;
+            Console.SetCursorPosition(inputStart, currentLine++);
+            var name = Console.ReadLine();
+            name = update && string.IsNullOrWhiteSpace(name) ? contact.Name : name;
+
+            Console.SetCursorPosition(inputStart, currentLine++);
+            var cellphone = Console.ReadLine() ?? contact.Cellphone.ToString();
+            cellphone = update && string.IsNullOrWhiteSpace(cellphone) ? contact.Cellphone.ToString() : cellphone;
+
+            Console.SetCursorPosition(inputStart, currentLine++);
+            var email = Console.ReadLine() ?? contact.Email.ToString();
+            email = update && string.IsNullOrWhiteSpace(email) ? contact.Email.ToString() : email;
+
+            Console.SetCursorPosition(inputStart, currentLine++);
+            var street = Console.ReadLine() ?? contact.Address.Street;
+            street = update && string.IsNullOrWhiteSpace(street) ? contact.Address.Street : street;
+
+            Console.SetCursorPosition(inputStart, currentLine++);
+            int number = 0;
+            var numberStr = Console.ReadLine();
+            numberStr = update && string.IsNullOrWhiteSpace(numberStr) ? contact.Address.Number.ToString() : numberStr;
+            int.TryParse(numberStr, out number);
+
+            Console.SetCursorPosition(inputStart, currentLine++);
+            var neighborhood = Console.ReadLine() ?? contact.Address.Neighborhood;
+            neighborhood = update && string.IsNullOrWhiteSpace(neighborhood) ? contact.Address.Neighborhood : neighborhood;
+
+            Console.SetCursorPosition(inputStart, currentLine++);
+            var city = Console.ReadLine() ?? contact.Address.City;
+            city = update && string.IsNullOrWhiteSpace(city) ? contact.Address.City : city;
+
+            Console.SetCursorPosition(inputStart, currentLine++);
+            var state = Console.ReadLine() ?? contact.Address.State;
+            state = update && string.IsNullOrWhiteSpace(state) ? contact.Address.State : state;
+
+            if (update)
+            {
+                try
+                {
+                    var contactUpdate = new Contact(contact.Id,
+                        name,
+                        new Cellphone(cellphone),
+                        new Email(email),
+                        new Address(street, number, neighborhood, city, state));
+
+                    return contactUpdate;
+                }
+                catch (Exception ex)
+                {
+                    ShowStatusBarMessage(ex.Message);
+                }
+            }
+
+            try
+            {
+                var contactAdd = Contact.New(name,
+                    new Cellphone(cellphone),
+                    new Email(email),
+                    new Address(street, number, neighborhood, city, state));
+
+                return contactAdd;
+            }
+            catch (Exception ex)
+            {
+                ShowStatusBarMessage(ex.Message);
+            }
+
+            return null;
         }
     }
 }
